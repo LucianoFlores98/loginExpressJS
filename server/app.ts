cda34d68399cfb7dbe7a91e37c35b5c915d02e5a
LucianoFlores98 <lucianoaf-98@hotmail.com.ar>
2024-09-30 17:37:08 -0300

    Cors error solved

--- server/app.ts
+++ server/app.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import authRoutes from './routes/auth.routes'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors({
+  origin: "http://localhost:3000" //Definimos de donde provendrán nuestras peticiones (Front)
+}))
 
 app.use(authRoutes)
 
